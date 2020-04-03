b10e34053241dcd8353aaefaeeef4f3b15c3b1c3
Serares <jianu.rares30@gmail.com>
2020-04-03 22:01:16 +0300

    feat: single post endpoint added

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbPass = require('./config/db_pass');
 const mongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
 
 //body-parser is used to parse incoming req bodys
 const feedRoutes = require('./routes/feed');
 
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, new Date().getTime() + '+' + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        // meaning the file can pass;
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 const app = express();
 //app.use(bodyParser.urlencoded()) -> used for x-www-form-urlencoded <form>
 app.use(bodyParser.json()) // good for application/json
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use((req, res, next) => {
     //cors res
     //this does not send a response just sets a Header
@@ -24,8 +48,15 @@ app.get('/', (req, res, next) => {
     res.status(200).json({ "message": "Salutare1" })
 })
 
+app.use((err, req, res, next) => {
+    //this is the error handling function
+    console.log(err);
+    const status = err.statusCode;
+    const message = err.message;
+    res.status(status).json({ message: message });
+})
 
-mongoose.connect(dbPass.mongoPass)
+mongoose.connect(dbPass.mongoPass())
     .then(result => {
         app.listen(8080);
     })
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,45 +1,105 @@
 const { validationResult } = require('express-validator/check');
-
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
     //important to send status codes
     //for client to know what to render
-    res.status(200).json({
-        posts: [
-            {
-                _id: 1,
-                title: "FirstPost1",
-                content: "Salutare",
-                imageUrl: './images/imagine1.png',
-                creator: {
-                    name: "The dude"
-                },
-                createdAt: new Date()
+
+    Post.find()
+        .then(posts => {
+            res.status(200).json({
+                message: "Posts found successfully",
+                posts: posts
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            // throw will not work in a promise here
+            // because it's async
+            //throw err
+            next(err);
+        })
+
 }
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     //isEmpty  is a validationResult function
     if (!errors.isEmpty()) {
-        return res.status(422).json({ message: 'Not valid', errors: errors.array() })
+        const error = new Error('Not valid');
+        error.statusCode = 422;
+        error.errors = errors.array();
+        //this will exit the function and go the the error handling function from express
+        // will work here because it's synchronous
+        throw error;
+        // return res.status(422).json({ message: 'Not valid', errors: errors.array() })
     }
-    console.log("Changed 1", req.body);
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statuscode = 422;
+        throw error;
+    }
+
+    const imageUrl = req.file.path.replace('\\', '/');
     //added by bodyParser
     const title = req.body.title;
     const content = req.body.content;
-    //here we'll get to the db
-    //201 success created resource
-    res.status(201).json({
-        message: "Post created success",
-        post: {
-            _id: new Date().getTime(),
-            title: title,
-            content: content,
-            creator: { name: "RJ" },
-            createdAt: new Date()
-        }
-    });
+    // no need for createdAt
+    // no need for _id ; mongoose will create thoes
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: imageUrl,
+        creator: { name: "RJ" }
+    })
+
+    post.save()
+        .then(response => {
+            //here we'll get to the db
+            //201 success created resource
+            res.status(201).json({
+                message: "Post created success",
+                post: post
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            // throw will not work in a promise here
+            // because it's async
+            //throw err
+            next(err);
+        })
+
+}
+
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    //if you return from a then
+    // the next then will be called with the returned value
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error("Cant find post");
+                error.statusCode = 404;
+                // if you throw err in .then
+                // it will get to the catch();
+                throw error;
+            }
+            res.status(200).json({ message: "Post found", post: post });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            // throw will not work in a promise here
+            // because it's async
+            //throw err
+            next(err);
+        })
+
 }
